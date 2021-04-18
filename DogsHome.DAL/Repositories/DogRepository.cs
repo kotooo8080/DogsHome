6977067927ddf191a76d8a50fd1bb29607141734
kotooo8080 <kotooo8080@gmail.com>
2021-04-19 02:31:26 +0300

    Добавлен Data Access Layer

---
+++ DogsHome.DAL/Repositories/DogRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using DogsHome.DAL.Context;
+using DogsHome.DAL.Entities;
+using DogsHome.DAL.Interfaces;
+
+namespace DogsHome.DAL.Repositories
+{
+    public class DogRepository : IRepository<Dog>
+    {
+        private DogsHomeContext db;
+
+        public DogRepository(DogsHomeContext context)
+        {
+            this.db = context;
+        }
+
+        public IEnumerable<Dog> GetAll()
+        {
+            return db.Dogs;
+        }
+
+        public Dog Get(int id)
+        {
+            return db.Dogs.Find(id);
+        }
+
+        public void Create(Dog dog)
+        {
+            db.Dogs.Add(dog);
+        }
+
+        public void Update(Dog dog)
+        {
+            db.Entry(dog).State = EntityState.Modified;
+        }
+
+        public IEnumerable<Dog> Find(Func<Dog, Boolean> predicate)
+        {
+            return db.Dogs.Where(predicate).ToList();
+        }
+
+        public void Delete(int id)
+        {
+            Dog dog = db.Dogs.Find(id);
+            if (dog != null)
+                db.Dogs.Remove(dog);
+        }
+    }
+}
---
+++ DogsHome.DAL/Repositories/DogsHomeUnitOfWOrk.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DogsHome.DAL.Context;
+using DogsHome.DAL.Entities;
+using DogsHome.DAL.Interfaces;
+
+namespace DogsHome.DAL.Repositories
+{
+    public class DogsHomeUnitOfWork : IUnitOfWork
+    {
+        private DogsHomeContext db;
+        private DogRepository dogRepository;
+        private DonationRepository donationRepository;
+
+        public DogsHomeUnitOfWork(string connectionString)
+        {
+            db = new DogsHomeContext(connectionString);
+        }
+        public IRepository<Dog> Dogs
+        {
+            get
+            {
+                if (dogRepository == null)
+                    dogRepository = new DogRepository(db);
+                return dogRepository;
+            }
+        }
+
+        public IRepository<Donation> Donations
+        {
+            get
+            {
+                if (donationRepository == null)
+                    donationRepository = new DonationRepository(db);
+                return donationRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
